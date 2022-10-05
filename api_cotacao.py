1a055a4057fa8be05e78ae9f3298fde57166e02e
Daniel Dantas <danieldantasoliv@gmail.com>
2022-10-05 02:12:27 -0300

    atualização 04/10/2022

--- api_cotacao.py
+++ api_cotacao.py
@@ -3,8 +3,20 @@ import json
 
 cotacoes = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL") # fazendo uma requisissão com a api de cotações (retorna um json)
 cotacoes = cotacoes.json() # convertendo json para dicionario do python
-cotacao_dolar = cotacoes['USDBRL']['bid']
+
+moeda = input("""
+    Digite umas das seguintes moedas para exibir a cotação em Real R$:
+    dollar
+    euro
+    bitcoin
+""")
 
 #print(cotacoes) # Exibindo o json na tela
-print(cotacao_dolar)
-
+if(moeda == 'dollar'):
+    print(f"Cotação do Dollar: {cotacoes['USDBRL']['bid']}")
+elif(moeda == 'euro'):
+    print(f"Cotação do Euro: {cotacoes['EURBRL']['bid']}")
+elif(moeda == 'bitcoin'):
+    print(f"Cotação do Bitcoin: {cotacoes['BTCBRL']['bid']}")
+else:
+    print('Opção Incorreta')
